54c311e0578eded0f6a63a784589b1155df703dd
iPapatsoris <giannhs95@yahoo.gr>
2017-05-07 00:22:10 +0300

    Refactor

--- Readme.md
+++ Readme.md
@@ -1,6 +1,8 @@
 # B+ Tree
 Low level B+ Tree data index, as practice for Implementation Of Database Systems class.
 
+Implemented in Fall 2015.
+
 ## Team
 * **Ioannis Papatsoris**: *bplus_tree* module
 * **Christos Vrachas**: *scans_management* module
--- util.h
+++
@@ -1,6 +0,0 @@
-int memswap(void *A, void *B, int len);
-int GrtOrEq(void *value1, void *value2, char type);
-int LesOrEq(void *value1, void *value2, char type);
-int Less(void *value1, void *value2, char type);
-int Greater(void *value1, void *value2, char type);
-int Equal(void *value1, void *value2, char type);
